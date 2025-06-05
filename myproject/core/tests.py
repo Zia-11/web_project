1fcbfce3edc32464ed58140092a9bfddcc183517
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2025-06-06 09:54:25 +1000

    preparing for the tests

--- myproject/core/tests.py
+++ myproject/core/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from .models import Item
+
+
+class ItemAPITestCase(APITestCase):
+    def setUp(self):
+        # создаём пользователя для аутентификации
+        self.username = "testuser"
+        self.password = "testpass123"
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password)
+
+        # получаем токен через эндпоинт /api-token-auth/
+        url_token = reverse('api_token_auth')
+        data = {"username": self.username, "password": self.password}
+        response = self.client.post(url_token, data, format='json')
+        # 200 и в ответе должен быть токен
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.token = response.data['token']
+
+        # создаём несколько item для проверки списка и фильтрации
+        Item.objects.create(title="First", description="First desc")
+        Item.objects.create(title="Second", description="Second desc")
+        Item.objects.create(title="Another", description="Another desc")
+
+        # готовим клиент для аутентифицированных запросов
+        self.auth_client = APIClient()
+        self.auth_client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
