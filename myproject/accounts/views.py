f295a1bba441aadafb88f2a63cb29de8f0ad1b02
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2025-06-08 15:20:10 +1000

    registered views

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.pagination import PageNumberPagination
+from .serializers import UserSerializer
 
 # Create your views here.
+
+User = get_user_model()
+
+# регистрация (доступна всем)
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+# список пользователей (требует аутентификации)
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PageNumberPagination
